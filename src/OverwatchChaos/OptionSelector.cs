536f24365cb49e3405dff2c7b0c54366a2dadefc
Aux <justin@akitaux.com>
2019-01-13 13:19:43 -0600

    Add basic random option selector

---
+++ src/OverwatchChaos/OptionSelector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverwatchChaos
+{
+    public class OptionSelector
+    {
+        private readonly Random _random;
+
+        public OptionSelector()
+        {
+            _random = new Random();
+        }
+
+        public string Choose(Dictionary<object, object> values)
+        {
+            var builder = new StringBuilder();
+            
+            var previous = values.ElementAtOrDefault(_random.Next(0, values.Count - 1));
+            while (true)
+            {
+                builder.AppendFormat("{0}:", previous.Key);
+                var dictionary = previous.Value as Dictionary<object, object>;
+                if (dictionary == null)
+                {
+                    object value;
+                    if (double.TryParse(previous.Value.ToString(), out double _))
+                        value = Math.Round(_random.NextDouble() * (5.0 - 0.1) - 0.1, 2);
+                    else
+                    if (bool.TryParse(previous.Value.ToString(), out bool b))
+                        value = !b;
+                    else
+                        value = previous.Value;
+                    
+                    builder.Append("\t" + value);
+                    break;
+                }
+                
+                int index = _random.Next(0, dictionary.Count - 1);
+                previous = dictionary.ElementAtOrDefault(index);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
--- src/OverwatchChaos/Program.cs
+++ src/OverwatchChaos/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 
 namespace OverwatchChaos
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var filepath = Path.Combine(AppContext.BaseDirectory, "default_heroes.yml");
+            var content = File.ReadAllText(filepath);
+            
+            var yaml = new DeserializerBuilder().Build();
+            var heroes = yaml.Deserialize<Dictionary<object, object>>(content);
+
+            var selector = new OptionSelector();
+            while (true)
+            {
+                string reply = selector.Choose(heroes);
+
+                Console.WriteLine(reply);
+                Console.ReadKey();
+            }
         }
     }
 }
