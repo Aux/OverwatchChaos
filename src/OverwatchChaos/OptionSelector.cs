04997e98cdd8a0e5efc0cefa87a9bb919ae96c60
Aux <justin@akitaux.com>
2019-01-20 11:13:54 -0600

    Add generation options, cleaner output

--- src/OverwatchChaos/OptionSelector.cs
+++ src/OverwatchChaos/OptionSelector.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace OverwatchChaos
 {
@@ -13,15 +12,15 @@ namespace OverwatchChaos
         {
             _random = new Random();
         }
-
-        public string Choose(Dictionary<object, object> values)
+        
+        public List<object> Choose(Dictionary<object, object> values)
         {
-            var builder = new StringBuilder();
-            
-            var previous = values.ElementAtOrDefault(_random.Next(0, values.Count - 1));
+            var choicePath = new List<object>();
+
+            var previous = values.ElementAtOrDefault(_random.Next(0, values.Count));
             while (true)
             {
-                builder.AppendFormat("{0}:", previous.Key);
+                choicePath.Add(previous.Key);
                 var dictionary = previous.Value as Dictionary<object, object>;
                 if (dictionary == null)
                 {
@@ -33,16 +32,47 @@ namespace OverwatchChaos
                         value = !b;
                     else
                         value = previous.Value;
-                    
-                    builder.Append("\t" + value);
+
+                    choicePath.Add(value);
                     break;
                 }
-                
+
                 int index = _random.Next(0, dictionary.Count - 1);
                 previous = dictionary.ElementAtOrDefault(index);
             }
 
-            return builder.ToString();
+            return choicePath;
+        }
+
+        public Dictionary<object, object> ChooseMany(Dictionary<object, object> values, int count)
+        {
+            var choices = new Dictionary<object, object>();
+
+            int hangupCounter = 0;
+            for (int n = 0; n < count; n++)
+            {
+                var path = Choose(values);
+
+                if (!choices.TryGetValue(path.First(), out object value))
+                    choices.Add(path.First(), new Dictionary<string, object>());
+
+                var option = choices[path.First()] as Dictionary<string, object>;
+                var key = string.Join(":", path.Skip(1).Take(path.Count - 2));
+                if (option.ContainsKey(key))
+                {
+                    if (hangupCounter > 25)
+                    {
+                        Console.WriteLine($"Ran out of options to generate after {n} iterations...\n");
+                        break;
+                    }
+                    hangupCounter++;
+                    n--;
+                    continue;
+                }
+                option.Add(key, path.Last().ToString());
+            }
+
+            return choices;
         }
     }
 }
--- src/OverwatchChaos/Program.cs
+++ src/OverwatchChaos/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using YamlDotNet.Serialization;
 
@@ -13,16 +14,47 @@ namespace OverwatchChaos
             var filepath = Path.Combine(AppContext.BaseDirectory, "default_heroes.yml");
             var content = File.ReadAllText(filepath);
             
-            var yaml = new DeserializerBuilder().Build();
-            var heroes = yaml.Deserialize<Dictionary<object, object>>(content);
+            var yamlreader = new DeserializerBuilder().Build();
+            var yamlwriter = new SerializerBuilder().Build();
+            var heroes = yamlreader.Deserialize<Dictionary<object, object>>(content);
 
+            Console.WriteLine($"Loaded {heroes.Count} heroes from config...\n");
+            Console.WriteLine($"Available heroes: {string.Join(", ", heroes.Select(x => x.Key))}\n");
             var selector = new OptionSelector();
             while (true)
             {
-                string reply = selector.Choose(heroes);
+                Console.WriteLine("Who should I generate options for? (press enter to skip) ");
+                var generateHeroes = Console.ReadLine().ToLower().Split(',', ' ').ToList();
 
-                Console.WriteLine(reply);
-                Console.ReadKey();
+                var validHeroes = heroes.Select(x => x.Key);
+                generateHeroes.RemoveAll(x => !validHeroes.Contains(x));
+
+                Dictionary<object, object> chosenHeroes;
+                if (generateHeroes.Count == 0)
+                {
+                    chosenHeroes = heroes;
+                    Console.WriteLine($"Generating options for: all\n");
+                }
+                else
+                {
+                    chosenHeroes = heroes.Where(x => generateHeroes.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value);
+                    Console.WriteLine($"Generating options for: {string.Join(", ", chosenHeroes.Select(x => x.Key))}\n");
+                }
+
+                Console.WriteLine($"How many options should I generate? ");
+                var input = Console.ReadLine();
+                Console.WriteLine();
+                if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine($"{input} is not a valid number, try again!");
+                    continue;
+                }
+
+                var choices = selector.ChooseMany(chosenHeroes, number);
+                Console.WriteLine(yamlwriter.Serialize(choices));
+                Console.WriteLine("Generate more? y/n");
+                var more = Console.ReadLine();
+                if (more.ToLower() == "n") break;
             }
         }
     }
